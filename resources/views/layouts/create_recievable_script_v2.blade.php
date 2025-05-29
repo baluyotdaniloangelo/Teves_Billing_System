6a826b48ba921727fe91a7b9462b1d5d80147570
baluyotdaniloangelo <baluyotdaniloangelo@gmail.com>
2025-05-29 08:10:54 +0800

    Update create_recievable_script_v2.blade.php

--- resources/views/layouts/create_recievable_script_v2.blade.php
+++ resources/views/layouts/create_recievable_script_v2.blade.php
@@ -16,7 +16,8 @@
 					
 			document.getElementById('generate_report_form').className = "g-3 needs-validation was-validated";
 
-			let client_idx 			= $('#client_name option[value="' + $('#client_id').val() + '"]').attr('data-id');
+			//let client_idx 			= $('#client_name option[value="' + $('#client_id').val() + '"]').attr('data-id');
+			let client_idx = $("#client_name option[value=\"" + $('#client_id').val() + "\"]").attr('data-id');
 			let start_date 			= $("input[name=start_date]").val();
 			let end_date 			= $("input[name=end_date]").val();
 			let less_per_liter 		= $("input[name=less_per_liter]").val();
@@ -200,7 +201,8 @@
 					
 			document.getElementById('generate_report_form').className = "g-3 needs-validation was-validated";
 
-			let client_idx 			= $("#client_name option[value='" + $('#client_id').val() + "']").attr('data-id');
+			//let client_idx 			= $("#client_name option[value='" + $('#client_id').val() + "']").attr('data-id');
+			let client_idx = $("#client_name option[value=\"" + $('#client_id').val() + "\"]").attr('data-id');
 			let start_date 			= $("input[name=start_date]").val();
 			let end_date 			= $("input[name=end_date]").val();
 			let less_per_liter 		= $("input[name=less_per_liter]").val();
@@ -640,7 +642,7 @@
 	
 	<!--Save New receivables->
 	$("#save-receivables").click(function(event){
-			alert();
+			alert('s');
 			event.preventDefault();
 			
 					/*Reset Warnings*/
