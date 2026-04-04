06f744e26a1e2627744d2daf97ec7a3fcf3c6346
baluyotdaniloangelo <baluyotdaniloangelo@gmail.com>
2026-04-04 22:40:27 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
 	{
     $schedule->call(function () {
         app(\App\Http\Controllers\EmailController::class)->sendUnbilledReport();
-    })->dailyAt('22:30');
+    })->dailyAt('09:30');
 	}
 
     /**
